0655a5441e55bf27ee8057973622c2bb8446895f
Nauris <nauris@inbox.lv>
2020-05-07 23:32:29 +0300

    test10

---
+++ src/credentials.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID             "test"
+#define WIFI_PASS             "test"
+#define MQTT_SERVER  "test.test.com"
+#define MQTT_PORT  1883
+#define MQTT_USER "" 
+#define MQTT_PASS "" 
